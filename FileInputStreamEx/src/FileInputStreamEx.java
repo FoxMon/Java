a03934c5b45c711e18c08b1bb48eea0f54d740c4
FoxMon <jjockrod@naver.com>
2019-06-19 02:19:23 +0900

    Java

--- FileInputStreamEx/src/FileInputStreamEx.java
+++ FileInputStreamEx/src/FileInputStreamEx.java
@@ -2,27 +2,24 @@ import java.io.*;
 
 public class FileInputStreamEx {
 	public static void main(String[] args) {
-		byte[] b = { 7, 51, 3, 4, -1, 24 };
+		byte[] b = new byte[6];
 		
 		try {
-			FileInputStream fin = new FileInputStream("c:\\Temp\\test.out");
-			int n = 0;
-			int c;
+			FileInputStream fis = new FileInputStream("C:\\Temp\\test.out");
 			
-			while((c = fin.read()) != -1) {
+			int c;
+			int n = 0;
+			
+			while((c = fis.read()) != -1) {
 				b[n] = (byte)c;
 				n++;
 			}
-			System.out.println("c\\Temp\\test.out에서 읽은 배열을 출력합니다.");
 			
-			for(int i = 0; i < b.length; i++) {
-				System.out.print(b[i] + " ");
-			}
-			System.out.println();
+			System.out.println("C:\\Temp\\test.out을 읽었습니다.");
 			
-			fin.close();
-		}catch(IOException e){
-			System.out.println("c\\Temp\\test.out에서 읽지 못했습니다. 경로명을 체크해보세요.");
+			fis.close();
+		} catch(IOException e) {
+			System.out.println("Error");
 		}
 	}
 }
