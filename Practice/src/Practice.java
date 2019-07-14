01c15501c8b513472aea1ae59089baed49c916b9
FoxMon <jjockrod@naver.com>
2019-07-14 22:44:39 +0900

    Java

--- Practice/src/Practice.java
+++ Practice/src/Practice.java
@@ -1,18 +1,19 @@
-public class Practice{
+public class Practice {
 	public static void main(String[] args) {
-		CountThread ct = new CountThread();
+		CounterThread ct = new CounterThread();
 		
-		ct.start();
-		
-		Thread ctr = new Thread(new CountRunnable());
-		
-		ctr.start();
-		
+		 ct.start();
+		 
+		 Thread cr = new Thread(new CounterRunnable());
+		 
+		 cr.start();
+		 
+		// 0 ~ 9까지 카운트
 		for(int i = 0; i < 10; i++) {
-			System.out.println("[" + Thread.currentThread().getName() + "]" + "Count : " + i);
+			System.out.println("[" + Thread.currentThread().getName() + "] " + "Count : " + i);
 			
 			if(i == 7) {
-				ct.clear();
+				ct.clearFlag();
 			}
 			
 			try {
@@ -23,18 +24,18 @@ public class Practice{
 		}
 	}
 	
-	private static class CountThread extends Thread{
-		boolean mflag = true;
+	private static class CounterThread extends Thread {
+		private boolean mflag = true;
 		
 		@Override
 		public void run() {
 			for(int i = 0; i < 10; i++) {
-				System.out.println("[" + getName() + "]" + "Count : " + i);
-				
 				if(mflag == false) {
 					return;
 				}
 				
+				System.out.println("[" + getName() + "] " + "Count : " + i);
+			
 				try {
 					Thread.sleep(1000);
 				} catch(InterruptedException e) {
@@ -43,24 +44,24 @@ public class Practice{
 			}
 		}
 		
-		public void clear() {
+		public void clearFlag() {
 			this.mflag = false;
 		}
 	}
 	
-	private static class CountRunnable implements Runnable{
+	private static class CounterRunnable implements Runnable {
 		
 		@Override
 		public void run() {
 			for(int i = 0; i < 10; i++) {
-				System.out.println("[" + Thread.currentThread().getName() + "]" + "Count : " + i);
-				
+				System.out.println("[" + Thread.currentThread().getName() + "] " + "Count : " + i);
+			
 				try {
 					Thread.sleep(1000);
 				} catch(InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
-		}
+		} 
 	}
 }
